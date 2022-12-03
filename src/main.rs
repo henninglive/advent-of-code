b7654ab1352742d6ee6dd046f50fd698861ddad3
Henning Ottesen <henning@live.no>
2022-12-03 17:34:23 +0100

    Switch from using structopt to using clap 4.0 for CLI parsing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
-extern crate core;
-
 mod year2020;
 mod year2021;
 mod year2022;
 
-use structopt::StructOpt;
+use clap::Parser;
 
 type Solution = (Option<fn() -> i64>, Option<fn() -> i64>);
 type Year = [Solution; 24];
@@ -15,44 +13,71 @@ static YEARS: [(u16, &'static Year); 3] = [
     (2022, &year2022::SOLUTIONS)
 ];
 
+#[derive(Parser, Debug)]
+#[command(author, version)]
+#[command(about = "Solutions Advent of Code code challenges: https://adventofcode.com")]
+#[command(long_about =
+"Solutions Advent of Code code challenges: https://adventofcode.com
 
-/// [Advent of Code](https://adventofcode.com) is a website with Advent Calendars
-/// with small programming puzzles for a variety of skill sets and skill levels that
-/// can be solved in any programming language.
-///
-/// This program contains solutions to some of the puzzles, solved using the
-/// [Rust programing language](https://www.rust-lang.org/en-US/).
-///
-#[derive(StructOpt, Debug)]
-#[structopt(name = "advent-of-code")]
-struct Opt {
+Advent of Code https://adventofcode.com is a website with Advent Calendars
+with small programming puzzles for a variety of skill sets and skill levels that
+can be solved in any programming language."
+)]
+struct Cli {
     /// Year
-    #[structopt()]
     year: Option<u16>,
 
-    /// Day of Advent
-    #[structopt()]
+    /// Day
     day: Option<u16>,
 }
 
-fn print_day(day: u16, solution: Solution) {
-    match solution {
-        (Some(part1), Some(part2)) => println!("Day {}: part1: {}, part2: {}", day, part1(), part2()),
-        (Some(part1), None) => println!("Day {}: part1: {}", day, part1()),
-        (None, Some(part2)) => println!("Day {}: part2: {}", day, part2()),
-        _ => {}
-    }
-}
-
 fn main() {
-    let opt: Opt = Opt::from_args();
-    for (y, days) in YEARS {
-        if opt.year.is_none() || opt.year == Some(y) {
-            println!("\nAdvent of Code {}:", y);
-            match opt.day {
-                Some(d) if d < 24 => print_day(d + 1, days[d as usize]),
-                _ => (0..24).for_each(|d| print_day(d + 1, days[d as usize]))
+    let cli: Cli = Cli::parse();
+
+    match cli.day {
+        Some(1..=24) | None => {}
+        Some(n) => {
+            eprintln!("Day {} out of range, must be between 1..24.", n);
+            return;
+        }
+    }
+
+    let years: &[(u16, &'static Year)] = match cli.year {
+        Some(year) => {
+            let year_solutions = YEARS
+                .iter()
+                .find(|solutions| solutions.0 == year)
+                .expect(format!("No solutions found for year {}", year).as_str());
+
+            std::slice::from_ref(year_solutions)
+        }
+        None => &YEARS[..]
+    };
+
+    for year in years {
+        println!("Year {}", year.0);
+
+        let days: Vec<(u16, &Solution)> = match cli.day {
+            Some(day) => {
+                let day_solutions = &year.1[(day - 1) as usize];
+                vec![(day, day_solutions)]
             }
+            None => year.1
+                .iter()
+                .enumerate()
+                .map(|(day_idx, solution)| ((day_idx + 1) as u16, solution))
+                .collect::<Vec<_>>()
+        };
+
+        for (day, solution) in days {
+            let solve = |of: Option<fn() -> i64>| -> String {
+                of.map(|f| f().to_string()).unwrap_or_else(|| "unsolved".to_string())
+            };
+
+            let part1 = solve(solution.0);
+            let part2 = solve(solution.1);
+
+            println!("\tDay: {:2}, part1: {:>10}, part2: {:>10}", day, part1, part2)
         }
     }
 }
